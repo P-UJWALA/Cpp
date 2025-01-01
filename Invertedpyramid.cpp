1329700c1c008e4318ed20be43078bbe4452d9df
P-UJWALA <164027329+P-UJWALA@users.noreply.github.com>
2025-01-01 22:18:33 +0530

    Add files via upload

---
+++ Invertedpyramid.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int rows;
+    cout << "Enter number of rows";
+    cin >> rows;
+    for(int i=0;i<rows;i++){
+        for(int j=rows;j>i;j--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
